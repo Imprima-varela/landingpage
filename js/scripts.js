424e35b261c40406c5d3773cf28e75074c3b2c00
Maurício Spagnol <mauriciogspagnol@gmail.com>
2022-06-25 21:28:10 -0300

    alt

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+
 /* Template: Juno - Multipurpose Landing Page Pack
    Author: humbleitThemes
    Created: Mar 2019
@@ -258,3 +259,52 @@
 	});
 
 })(jQuery);
+
+
+
+/*Slide*/
+const items = document.querySelectorAll('.img-slide');
+const itemCount = items.length;
+const nextItem = document.querySelector('.next-slide');
+const previousItem = document.querySelector('.previous-slide');
+let count = 0;
+
+function showNextItem() {
+  items[count].classList.remove('active');
+
+  if(count < itemCount - 1) {
+    count++;
+  } else {
+    count = 0;
+  }
+
+  items[count].classList.add('active');
+  console.log(count);
+}
+
+function showPreviousItem() {
+  items[count].classList.remove('active');
+
+  if(count > 0) {
+    count--;
+  } else {
+    count = itemCount - 1;
+  }
+
+  items[count].classList.add('active');
+  console.log(count);
+}
+
+function keyPress(e) {
+  e = e || window.event;
+  
+  if (e.keyCode == '37') {
+    showPreviousItem();
+  } else if (e.keyCode == '39') {
+    showNextItem();
+  }
+}
+
+nextItem.addEventListener('click', showNextItem);
+previousItem.addEventListener('click', showPreviousItem);
+document.addEventListener('keydown', keyPress);
